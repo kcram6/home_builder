df9d165f0b8a90129c7f2cfe82ca1ea483f43c91
Kathleen Cram <d00297334@dmail.dixie.edu>
2019-02-26 12:12:25 -0700

    some structure and style

--- client/app.js
+++ client/app.js
@@ -1,10 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'hi'
+        // landingRegisterButton: false,
+        // landingSignInButton: false,
+        plans : [
+            hi,
+            hi
+        ]
     },
     methods: {
+        // registerClicked: function() {
+        //     landingRegisterButton = true;
+        //     console.log('register clicked');
+        // },
 
+        // signInClicked: function() {
+        //     landingSignInButton = true;
+        //     console.log('sign in clicked');
+        // }
     },
     created: function() {
         console.log('created app');
