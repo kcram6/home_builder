2aea2aa0944b57981a07b17692cb973d3f1b4453
Kathleen Cram <d00297334@dmail.dixie.edu>
2019-02-27 13:18:05 -0700

    broken v show

--- client/app.js
+++ client/app.js
@@ -7,23 +7,32 @@ var app = new Vue({
         showChoosePlan: false,
         showUserBase: false,
         
-        plans : [
-        ]
+
+        
+        plans : []
     },
     methods: {
         addPlan: function() {
             console.log('button clicked');
+        },
+
+        registerClicked: function() {
+            this.showLandingPage = false;
+            this.showRegisterPage = true;
+            this.showSignInPage = false;
+            this.showUserBase = false;
+            this.showChoosePlan = false;
+            console.log('register clicked');
+        },
+
+        signInClicked: function() {
+            this.showSignInPage = true;
+            this.showLandingPage = false;
+            this.showRegisterPage = false;
+            this.showChoosePlan = false;
+            this.showUserBase = false;
+            console.log('sign in clicked');
         }
-
-        // registerClicked: function() {
-        //     landingRegisterButton = true;
-        //     console.log('register clicked');
-        // },
-
-        // signInClicked: function() {
-        //     landingSignInButton = true;
-        //     console.log('sign in clicked');
-        // }
     },
     created: function() {
         console.log('created app');
